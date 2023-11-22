1f795f32cfbbde5cb8bcf10debbb7cca8f91de79
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2023-11-22 23:37:46 +0700

    Initial commmit

---
+++ server/modules/customer/customer.controller.ts
@@ -0,0 +1,68 @@
+import { Customer } from "../../entity/Customer";
+import {
+  Get,
+  Post,
+  Body,
+  Controller,
+  Query,
+  Param,
+  Put,
+  Delete
+} from "routing-controllers";
+import {
+  PaginationInfo,
+  IPaginationQueryParam
+} from "../../decorators/PaginationInfo";
+import { CrudServices, IFetchPageQuery } from "../../services/CrudServices";
+import { CurrentUser } from "../../decorators/CurrentUser";
+
+@Controller("/customers")
+export class CustomersController {
+  private crudServices: CrudServices<Customer>;
+
+  constructor() {
+    this.crudServices = new CrudServices<Customer>();
+    this.crudServices.setEntity(Customer);
+  }
+
+  @Get("/:id")
+  public async getCustomerById(@Param("id") id: string): Promise<any> {
+    const res = await this.crudServices.fetchById(id);
+    return res || {};
+  }
+
+  @Get()
+  public async getCustomers(
+    @PaginationInfo() paginationInfo: IPaginationQueryParam,
+    @Query("q") search?: string
+  ): Promise<Customer[]> {
+    const query: IFetchPageQuery = {
+      search,
+      perPage: paginationInfo.perPage,
+      page: paginationInfo.pageNo
+    };
+    return await this.crudServices.fetchPages(query);
+  }
+
+  @Post()
+  public async createNewCustomer(
+    @Body() Customer: Customer,
+    @CurrentUser() userid: string
+  ): Promise<any> {
+    return await this.crudServices.create(userid, Customer);
+  }
+
+  @Put("/:id")
+  public async updateCustomer(
+    @Param("id") id: string,
+    @Body() data: Customer,
+    @CurrentUser() userid: string
+  ) {
+    return await this.crudServices.updateById(userid, { id }, data);
+  }
+
+  @Delete("/:id")
+  public async deleteCustomer(@Param("id") id: string): Promise<any> {
+    return await this.crudServices.deleteById(id);
+  }
+}
